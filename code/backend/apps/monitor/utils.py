5c000376794352fc6ed919a5e6431766b9aaa3b8
yuchuan <13186087857@163.com>
2025-10-26 21:04:21 +0800

    optimize

--- code/backend/apps/monitor/utils.py
+++ code/backend/apps/monitor/utils.py
@@ -1,15 +1,23 @@
-from apps.monitor.models import Node
+from apps.monitor.models import Link, Node
 
+def format_link_data(link: Link):
+    """格式化链路数据"""
+    res = {
+        'uuid': str(link.uuid),
+        'name': link.name
+    }
+
+    if link.is_del:
+        res['name'] = link.name + '[已删除]'
+
+    return res
 
 def format_node_data(node: Node):
     """格式化节点数据"""
     res = {
         'uuid': str(node.uuid),
         'name': node.name,
-        'link': {
-            'uuid': str(node.link.uuid),
-            'name': node.link.name
-        }
+        'link': format_link_data(node.link)
     }
 
     if node.is_del:
